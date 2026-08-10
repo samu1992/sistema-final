c52133aa3275f5cb228e9f970fa02918e17d4d87
samuel carrizo <sam@Samuel.localdomain>
2026-08-10 16:58:08 +0000

    feat: despliegue automatizado en EKS con auto-escalado HPA

---
+++ terraform/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "us-east-2" 
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name = "bot-latam-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-2a", "us-east-2b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0" # SALTO A LA VERSIÓN 20 (COMPATIBLE CON AWS v5)
+
+  cluster_name    = "cluster-bot-trading"
+ #cluster_version = "1.29" # Versión de K8s actualizada
+
+  # Habilitamos el acceso desde tu GitHub Actions
+  cluster_endpoint_public_access  = true
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.public_subnets
+
+  eks_managed_node_groups = {
+    nodos_bot = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+      instance_types = ["t3.medium"]
+    }
+  }
+}
